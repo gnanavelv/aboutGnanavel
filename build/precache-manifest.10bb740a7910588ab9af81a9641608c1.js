46c9c6f35413c051d9892117bb0a02ec9cad89b2
gnanavelv <gnanavelv27@gmail.com>
2019-02-10 20:53:57 +0530

    fifth

---
+++ build/precache-manifest.10bb740a7910588ab9af81a9641608c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "52d4a52b4f5f8e6820b8178ee30ffa2d",
+    "url": "/aboutGnanavel/static/media/darkbg.52d4a52b.jpg"
+  },
+  {
+    "revision": "1207cbb6c534c8185468ddc5df7c8d13",
+    "url": "/aboutGnanavel/static/media/GNANAVEL-Resume.1207cbb6.pdf"
+  },
+  {
+    "revision": "b379e07aeced0bd0ec86",
+    "url": "/aboutGnanavel/static/js/runtime~main.b379e07a.js"
+  },
+  {
+    "revision": "358185f2689a61e6c7c0",
+    "url": "/aboutGnanavel/static/js/main.358185f2.chunk.js"
+  },
+  {
+    "revision": "a5b61b2bfd0a365d4609",
+    "url": "/aboutGnanavel/static/js/1.a5b61b2b.chunk.js"
+  },
+  {
+    "revision": "358185f2689a61e6c7c0",
+    "url": "/aboutGnanavel/static/css/main.29714eab.chunk.css"
+  },
+  {
+    "revision": "91ef323418020d7de74b71d030fcaf34",
+    "url": "/aboutGnanavel/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -18,11 +18,11 @@ class App extends Component {
   <div className="row main-row">
      <div className="col-lg-8 col-lg-2">
       <p className="welcome">Hi, I am Gnanavel. </p> 
-      <p className="github-text">I am a Full-Stack developer and a big fan of the open source community also love building new projects. My coding work can be seen on my <a href="https://github.com/Gnanu27" rel="noopener noreferrer" target="_blank">Github</a>.</p>
+      <p className="github-text">I am a Full-Stack developer and a big fan of the open source community, also love building new projects. My coding work can be seen on my <a href="https://github.com/Gnanu27" rel="noopener noreferrer" target="_blank">Github</a>.</p>
       <p className="youtube-text">I have passion for creating machine learning models and building web applications.</p>
       <p className="social-text">Bad at sarcasm and above average at Programming</p>
-      <p className="linkedin-text">Vist my <a href="https://linkedin.com/in/gnanavelv27" rel="noopener noreferrer" target="_blank">Linkedin</a> profile. If you add me don't expect an instant response, I don't usually check it.</p>
-      <p className="resume-text">Lastly, my last updated <a target="_blank" href={Resume} rel="noopener noreferrer">resume</a> (Feburary 2019).</p>
+      <p className="linkedin-text">Visit my <a href="https://linkedin.com/in/gnanavelv27" rel="noopener noreferrer" target="_blank">Linkedin</a> profile. If you add me don't expect an instant response, I don't usually check it.</p>
+      <p className="resume-text">Lastly, my last updated <a target="_blank" href={Resume} rel="noopener noreferrer">resume</a> (February 2019).</p>
       <p className="contact-text">Contact: gnanavel.py2k@gmail.com</p>
     </div>
   </div>
