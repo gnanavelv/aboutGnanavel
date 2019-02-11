4b7a263ea3988e092dd6af3c374cfcca9dd0c255
gnanavelv <gnanavelv27@gmail.com>
2019-02-11 16:48:29 +0530

    Updates

---
+++ precache-manifest.17ae982b2ce6673b9e7595c98759822c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "52d4a52b4f5f8e6820b8178ee30ffa2d",
+    "url": "/aboutGnanavel/static/media/darkbg.52d4a52b.jpg"
+  },
+  {
+    "revision": "1207cbb6c534c8185468ddc5df7c8d13",
+    "url": "/aboutGnanavel/static/media/GNANAVEL-Resume.1207cbb6.pdf"
+  },
+  {
+    "revision": "50b3089073123d695b54",
+    "url": "/aboutGnanavel/static/js/runtime~main.50b30890.js"
+  },
+  {
+    "revision": "2acd98be439118d6c750",
+    "url": "/aboutGnanavel/static/js/main.2acd98be.chunk.js"
+  },
+  {
+    "revision": "6a33b94fd0fe2834b859",
+    "url": "/aboutGnanavel/static/js/2.6a33b94f.chunk.js"
+  },
+  {
+    "revision": "2acd98be439118d6c750",
+    "url": "/aboutGnanavel/static/css/main.9a0f465f.chunk.css"
+  },
+  {
+    "revision": "e3cad5d970ab48e0575b6ad81593c89d",
+    "url": "/aboutGnanavel/index.html"
+  }
+];
